b7195df5624bafce0c7ff9b91a5f0f3eeab59a59
alxsaunders <alexsaunders242@gmail.com>
2022-07-26 22:49:24 -0400

    add connect index and server js

---
+++ db/connection.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql12")
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3006,
+    user: "root",
+    password: "rootroot",
+    database: "employee"
+  });
+  
+ 
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("\nWelcome to the Employee Management System!\n");
+    startEmployeeManager();
+    updateServer();
+  });
+
+  module.exports = connection;
